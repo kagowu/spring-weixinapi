494e26caa970696184c82bdec68ea8088ea774e2
gong.hua <gong.hua@qiaofang.info>
2019-03-01 11:40:02 +0800

    增加加解密方法

---
+++ weixin-feignclient/src/main/java/com/qq/weixin/cipher/aes/AES_CBC.java
@@ -0,0 +1,57 @@
+package com.qq.weixin.cipher.aes;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.AlgorithmParameters;
+import java.security.Key;
+import java.security.Security;
+
+/**
+ * AES加密
+ *
+ * @link {http://www.cnblogs.com/happyhippy/archive/2006/12/23/601353.html}
+ */
+public class AES_CBC {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * AES解密
+     *
+     * @param content 密文
+     * @return
+     * @throws Exception
+     */
+    public byte[] decrypt(byte[] content, byte[] keyByte, byte[] ivByte) throws Exception {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            Key sKeySpec = new SecretKeySpec(keyByte, "AES");
+            // 初始化
+            cipher.init(Cipher.DECRYPT_MODE, sKeySpec, generateIV(ivByte));
+            byte[] result = cipher.doFinal(content);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * 生成iv （初始化向量)
+     *
+     * @param iv
+     * @return
+     * @throws Exception
+     */
+    public static AlgorithmParameters generateIV(byte[] iv) throws Exception {
+        AlgorithmParameters params = AlgorithmParameters.getInstance("AES");
+        params.init(new IvParameterSpec(iv));
+        return params;
+    }
+}
